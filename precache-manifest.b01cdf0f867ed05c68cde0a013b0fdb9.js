bd533d57a9ed97489d209e3e104a4e80ebce03bc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-15 11:45:50 +0000

    Deploy taeukme/covid-19-mask-map to github.com/taeukme/covid-19-mask-map.git:gh-pages

---
+++ precache-manifest.b01cdf0f867ed05c68cde0a013b0fdb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd75b6d6435f91c5f089605a0b984606",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dacd758543715f503ab0",
+    "url": "/static/css/main.465742cf.chunk.css"
+  },
+  {
+    "revision": "7a1fd3d9c5033951dd8b",
+    "url": "/static/js/2.66cadc54.chunk.js"
+  },
+  {
+    "revision": "ab38a95fb47bf3b6cd804889ab6f8ef8",
+    "url": "/static/js/2.66cadc54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dacd758543715f503ab0",
+    "url": "/static/js/main.05ea812d.chunk.js"
+  },
+  {
+    "revision": "b51cd6df43632122f054",
+    "url": "/static/js/runtime-main.7a7f8f7e.js"
+  },
+  {
+    "revision": "146663f2f33372c6a97885d0e874ebe6",
+    "url": "/static/media/MaskMapIntro.146663f2.svg"
+  },
+  {
+    "revision": "b988c10966c28ad3078daec2313485bf",
+    "url": "/static/media/MaskMapLogo.b988c109.svg"
+  }
+]);
